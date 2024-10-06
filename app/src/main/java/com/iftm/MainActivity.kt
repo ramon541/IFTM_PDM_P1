57de98f195cdcc74339aa713b59bc6137c25054c
ramon541 <ramondiasmonteiro@gmail.com>
2024-10-06 01:15:22 -0300

    :sparkles: feat: Edição e exclusão completos

--- app/src/main/java/com/iftm/MainActivity.kt
+++ app/src/main/java/com/iftm/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.iftm
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
@@ -22,7 +24,6 @@ class MainActivity : AppCompatActivity() {
     lateinit var bt_atualizar : Button
     lateinit var bt_excluir   : Button
     lateinit var lv_cursos    : ListView
-    lateinit var rb_area      : RadioButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -44,47 +45,113 @@ class MainActivity : AppCompatActivity() {
         showCursos(dao)
         insertMode()
 
+        //--------------------------------------------------------------------------------------
         bt_inserir.setOnClickListener {
             if (isInputsValid()) {
                 val selectedOption = rg_areas.checkedRadioButtonId
-                val rbArea = findViewById<RadioButton>(selectedOption)
+                val rbArea         = findViewById<RadioButton>(selectedOption)
+
                 val curso = Curso(
-                    0,
-                    et_nome.text.toString(),
-                    et_nAlunos.text.toString().toInt(),
-                    et_notaMEC.text.toString().toFloat(),
-                    rbArea.text.toString()
+                    0
+                    , et_nome   .text.toString()
+                    , et_nAlunos.text.toString().toInt()
+                    , et_notaMEC.text.toString().toFloat()
+                    , rbArea    .text.toString()
                 )
 
                 dao.insert(curso)
                 showCursos(dao)
                 clearInputs()
 
-                Toast.makeText(applicationContext, "Curso inserido!", Toast.LENGTH_LONG).show()
+                Toast.makeText(
+                      applicationContext
+                    , "Curso inserido!"
+                    , Toast.LENGTH_LONG
+                ).show()
             } else {
                 Toast.makeText(
-                    applicationContext,
-                    "Preencha todos os campos para poder inserir!",
-                    Toast.LENGTH_LONG
+                    applicationContext
+                    , "Preencha todos os campos para poder inserir!"
+                    , Toast.LENGTH_LONG
                 ).show()
             }
         }
 
-        bt_atualizar.setOnClickListener {
+        //--------------------------------------------------------------------------------------
+        lv_cursos.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
+            editMode()
+            val text : String = parent.getItemAtPosition(position).toString()
+            val separateData  = text.split(" - ").toTypedArray()
 
+            val tagToFind = separateData[4].split(": ")[1][0]
+            for (i in 0 until rg_areas.childCount) {
+                val radioButton = rg_areas.getChildAt(i) as RadioButton
+
+                if (radioButton.tag.equals(tagToFind.toString())) {
+                    rg_areas.check(radioButton.id)
+                    break
+                }
+            }
+
+            et_codigo .setText(separateData[0].split(": ")[1])
+            et_nome   .setText(separateData[1].split(": ")[1])
+            et_nAlunos.setText(separateData[2].split(": ")[1])
+            et_notaMEC.setText(separateData[3].split(": ")[1])
         }
 
-        bt_excluir.setOnClickListener {
+        //--------------------------------------------------------------------------------------
+        bt_atualizar.setOnClickListener {
+            if (isInputsValid()) {
+                val selectedOption = rg_areas.checkedRadioButtonId
+                val rbArea = findViewById<RadioButton>(selectedOption)
+                val curso = Curso(
+                      et_codigo .text.toString().toInt()
+                    , et_nome   .text.toString()
+                    , et_nAlunos.text.toString().toInt()
+                    , et_notaMEC.text.toString().toFloat()
+                    , rbArea    .text.toString()
+                )
 
+                dao.update(curso)
+                showCursos(dao)
+                clearInputs()
+
+                Toast.makeText(
+                      applicationContext
+                    , "Curso editado!"
+                    , Toast.LENGTH_LONG
+                ).show()
+            } else {
+                Toast.makeText(
+                      applicationContext
+                    , "Preencha todos os campos para poder editar!"
+                    , Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+
+        //--------------------------------------------------------------------------------------
+        bt_excluir.setOnClickListener {
+            dao.delete(et_codigo.text.toString())
+            showCursos(dao)
+            clearInputs()
+
+            Toast.makeText(
+                  applicationContext
+                , "Curso deletado!"
+                , Toast.LENGTH_LONG
+            ).show()
         }
     }
 
+    //--------------------------------------------------------------------------------------
     private fun showCursos(dao : DAO) {
         var cursosList    = dao.getAll()
         var adapter       = ArrayAdapter(applicationContext, android.R.layout.simple_list_item_1, cursosList)
         lv_cursos.adapter = adapter
     }
 
+    //--------------------------------------------------------------------------------------
     private fun clearInputs() {
         et_codigo .text.clear()
         et_nome   .text.clear()
@@ -95,6 +162,7 @@ class MainActivity : AppCompatActivity() {
         insertMode()
     }
 
+    //--------------------------------------------------------------------------------------
     private fun insertMode() {
         et_codigo.isEnabled = false
 
@@ -110,6 +178,7 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    //--------------------------------------------------------------------------------------
     private fun editMode() {
         bt_inserir  .setBackgroundColor(ContextCompat.getColor(this, com.google.android.material.R.color.material_on_background_disabled))
         bt_inserir  .isEnabled = false
@@ -121,6 +190,7 @@ class MainActivity : AppCompatActivity() {
         bt_excluir  .isEnabled = true
     }
 
+    //--------------------------------------------------------------------------------------
     private fun isInputsValid(): Boolean {
         val selectedOption = rg_areas.checkedRadioButtonId
 
